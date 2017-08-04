c6dd794e09027d32c1a83e8379db8a0ed7b904ed
S <selvan.s@capgemini.com>
2017-08-04 07:59:33 +0530

    Signed-off-by: S <selvan.s@capgemini.com>

---
+++ Cik.MagazineWeb.WebApp/app/app.js
@@ -0,0 +1,25 @@
+﻿// did some namespaces for the main app
+var magazinesManager = {};
+magazinesManager.magazinesApp = {};
+
+(function () {
+    'use strict';
+    magazinesManager.magazinesApp = angular.module('magazinesApp',
+        ['ngRoute', 'routeResolverServices']);
+    magazinesManager.magazinesApp.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/dashboard', {
+                controller: 'DashboardController',
+                templateUrl: '/app/views/dashboard.html'
+            })
+            .when('/categories', {
+                controller: 'CategoriesController',
+                templateUrl: '/app/views/category/categories.html'
+            })
+            .when('/items', {
+                controller: 'ItemsController',
+                templateUrl: '/app/views/item/items.html'
+            })
+            .otherwise({ redirectTo: '/dashboard' });
+    }]);
+}());
