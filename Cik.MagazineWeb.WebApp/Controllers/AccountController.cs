c0b8a79a80c9fe2aaba0b4f41bab2fc50af2aa21
S <selvan.s@capgemini.com>
2017-08-05 19:29:32 +0530

    Signed-off-by: S <selvan.s@capgemini.com>

--- Cik.MagazineWeb.WebApp/Controllers/AccountController.cs
+++ Cik.MagazineWeb.WebApp/Controllers/AccountController.cs
@@ -13,7 +13,7 @@
 
     [Authorize]
     public class AccountController : Controller
-    {
+    { 
         public AccountController()
             : this(new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
         {
